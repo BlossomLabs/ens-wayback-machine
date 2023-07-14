607045789fa4d6d55971333c94fb5cde77fe6b8f
DESKTOP-O2EM74E\jordi <hola@jordiplanas.cat>
2023-07-14 22:59:58 +0200

    obtain content from utils function

--- src/pages/[url]/index.tsx
+++ src/pages/[url]/index.tsx
@@ -10,7 +10,6 @@ import { Base58 } from '@ethersproject/basex';
 import { toUtf8String } from '@ethersproject/strings';
 
 // Functions
-import { getFromENSGraph } from "@/utils/ENSGraph";
 import { getDomainId } from "@/utils/domainId";
 
 import Timeline from "../../components/Timeline";
@@ -73,27 +72,12 @@ export default function PageViewer() {
           return result
         })
 
-        // Get content hashes
-        const encoded = await getContentHashes(resolverId).then((result) => {
-          return result
+        // Get content
+        await getContentHashes(resolverId).then((result) => {
+          setSnapshots(result.decodedWithDate)
+          setUrl(result.url)
         })
 
-        // Decode content hashes
-        const decoded = encoded.map(({ hash, blockNumber }: { hash: string; blockNumber: number }) => ({
-          hash: decode(hash),
-          blockNumber
-        }));
-
-        const decodedWithDate = await Promise.all(
-          decoded.map(async (obj: any) => {
-            const block = await ethereumProvider.getBlock(obj.blockNumber);
-            return { ...obj, date: block.timestamp };
-          })
-        );
-
-        setSnapshots(decodedWithDate);
-        setUrl(decodedWithDate[decoded.length - 1].hash);
-      
         // Get domainId
         await getDomainId(_url).then((result) => {
           setDomainId(result)
@@ -112,31 +96,37 @@ export default function PageViewer() {
     }
   }, [_url, domainId]);
 
-  return (
-    <Box
-      position={'relative'}
-    >
+
+  if(data) {
+    return (
       <Box
-        bg='primary.100'>
-        <Flex
-          px='30px'
-          pt='15px'
-          alignItems="center"
-          direction={['column', 'column', 'row']}
-        >
-          <Box minWidth="120px">
-            <Link to='/'>
-              <Box mb={5}>
-                <Image alt="Logo" src="/header-logo.svg" width="120" height="100" />
-              </Box>
-            </Link>
-          </Box>
-          <Box width="100%">
-            <Timeline data={data} onItemSelected={handleSnapshotChange} activeItem={url} />
-          </Box>
-        </Flex>
-        <iframe width="100%" style={{ minHeight: "100vh", border: 0 }} src={url ? url : ''} />
+        position={'relative'}
+      >
+        <Box
+          bg='primary.100'>
+          <Flex
+            px='30px'
+            pt='15px'
+            alignItems="center"
+            direction={['column', 'column', 'row']}
+          >
+            <Box minWidth="120px">
+              <Link to='/'>
+                <Box mb={5}>
+                  <Image alt="Logo" src="/header-logo.svg" width="120" height="100" />
+                </Box>
+              </Link>
+            </Box>
+            <Box width="100%">
+              <Timeline data={data} onItemSelected={handleSnapshotChange} activeItem={url} />
+            </Box>
+          </Flex>
+          <iframe width="100%" style={{ minHeight: "100vh", border: 0 }} src={url ? url : ''} />
+        </Box>
       </Box>
-    </Box>
-  );
+    );
+  } else {
+    // Handle screen when there is no data (user inputs url param)
+  }
+
 }
--- src/utils/contentHashes.tsx
+++ src/utils/contentHashes.tsx
@@ -1,4 +1,29 @@
 import { getFromENSGraph } from "./ENSGraph";
+import { StaticJsonRpcProvider } from '@ethersproject/providers'
+import contentHash from 'content-hash'
+import { Base58 } from '@ethersproject/basex';
+import { toUtf8String } from '@ethersproject/strings';
+
+function decode(encoded: string) {
+    if (!encoded.startsWith('0xe3') && !encoded.startsWith('0xe5')) {
+      // Not an IPFS or IPNS link
+      return ''
+    }
+    const ipfsv0 = contentHash.decode(encoded)
+  
+    if (encoded.startsWith('0xe5')) {
+      try {
+        return `https://` + toUtf8String(Base58.decode(ipfsv0).slice(2))
+      } catch (e) {
+        return ''
+      }
+    }
+    const ipfsv1 = contentHash.helpers.cidV0ToV1Base32(ipfsv0)
+    return `https://${ipfsv1}.ipfs.dweb.link/`
+  }
+
+const ethereumProvider = new StaticJsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/BZwin08uUdw6bSIy5pvWnglh7EXeQo64')
+
 
 export const getContentHashes = async(resolverId: string) => {
     return getFromENSGraph(
@@ -9,5 +34,24 @@ export const getContentHashes = async(resolverId: string) => {
       }
     }`,
     { resolverId },
-    (result: any) => result.data.contenthashChangeds
+    async ( result: any ) => {
+        // Decode content hashes
+        const decodedHashes = result.data.contenthashChangeds.map(({ hash, blockNumber }: { hash: string; blockNumber: number }) => ({
+          hash: decode(hash),
+          blockNumber
+        }));
+
+        // Include date
+        const decodedWithDate = await Promise.all(
+            decodedHashes.map(async (obj: any) => {
+              const block = await ethereumProvider.getBlock(obj.blockNumber);
+              return { ...obj, date: block.timestamp };
+            })
+          );
+
+        return {
+            decodedWithDate: decodedWithDate,
+            url: decodedWithDate[decodedHashes.length - 1].hash
+        }
+    }
   );}
