2e4899565c550a0f79146e0e83a9d64883d3804d
DESKTOP-O2EM74E\jordi <hola@jordiplanas.cat>
2023-07-14 23:03:25 +0200

    removed unnecessary imports

--- src/pages/[url]/index.tsx
+++ src/pages/[url]/index.tsx
@@ -1,13 +1,9 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { StaticJsonRpcProvider } from '@ethersproject/providers'
-import contentHash from 'content-hash'
 import { Box, Flex } from "@chakra-ui/react"
 import { Link, useParams } from "react-router-dom";
 import Image from 'next/image';
-import { Base58 } from '@ethersproject/basex';
-import { toUtf8String } from '@ethersproject/strings';
 
 // Functions
 import { getDomainId } from "@/utils/domainId";
@@ -16,26 +12,6 @@ import Timeline from "../../components/Timeline";
 import { getTransfersAndWrappedTransfers } from "@/utils/transfers";
 import { getResolverId } from "@/utils/resolverId";
 import { getContentHashes } from "@/utils/contentHashes";
-
-const ethereumProvider = new StaticJsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/BZwin08uUdw6bSIy5pvWnglh7EXeQo64')
-
-function decode(encoded: string) {
-  if (!encoded.startsWith('0xe3') && !encoded.startsWith('0xe5')) {
-    // Not an IPFS or IPNS link
-    return ''
-  }
-  const ipfsv0 = contentHash.decode(encoded)
-
-  if (encoded.startsWith('0xe5')) {
-    try {
-      return `https://` + toUtf8String(Base58.decode(ipfsv0).slice(2))
-    } catch (e) {
-      return ''
-    }
-  }
-  const ipfsv1 = contentHash.helpers.cidV0ToV1Base32(ipfsv0)
-  return `https://${ipfsv1}.ipfs.dweb.link/`
-}
 
 export default function PageViewer() {
   const [snapshots, setSnapshots] = useState<{ hash: string; date: number }[]>([]);
@@ -85,6 +61,8 @@ export default function PageViewer() {
 
         // Get Wrapped Transfers
         await getTransfersAndWrappedTransfers(domainId).then((result) => {
+          // Without console log it does not work. Why?
+          console.log(result)
           setWrappedTransfers(result)
         })
       })();
