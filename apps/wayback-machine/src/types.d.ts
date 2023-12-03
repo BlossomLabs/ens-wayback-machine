ee024622e652f045fb37c36942678c1bf1e52571
Sem <931684+sembrestels@users.noreply.github.com>
2023-12-03 11:29:47 +0100

    Remove unneded types file

--- apps/wayback-machine/src/types.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@ensdomains/content-hash' {
-  export const decode: (hash: string) => string;
-  export const cidForWeb: (hash: string) => string;
-  export const getCodec: (hash: string) => string;
-}
